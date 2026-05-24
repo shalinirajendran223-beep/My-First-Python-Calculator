a2a6e93ad158b1d48b1a5b814ee0ee1b7fc505a8
shalinirajendran223-beep <shalinirajendran223@gmail.com>
2026-05-24 22:54:30 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Cannot divide by zero."
+
+num1 = float(input("Enter first number: "))
+operation = input("Enter operation (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operation == '+':
+    print("Result:", add(num1, num2))
+elif operation == '-':
+    print("Result:", subtract(num1, num2))
+elif operation == '*':
+    print("Result:", multiply(num1, num2))
+elif operation == '/':
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid Operation")
+  
